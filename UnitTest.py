e36776f5687221c2f16dd1ba9024005d6c4d7bf4
Long Tong <longhtong@berkeley.edu>
2020-05-20 19:37:39 -0700

    start getPrime

--- UnitTest.py
+++ UnitTest.py
@@ -1,5 +1,6 @@
 import encryption as en
 import decryption as de
-
+import Utils as utils
 print(en.encrypt(13, 55, 3))
 print(de.decrypt(en.encrypt(13, 55, 3),3,5,11))
+print(utils.getPrime())
--- Utils.py
+++ Utils.py
@@ -1,5 +1,18 @@
+import random
+
 def ASCIIConvert(message):
     result = 0
     for i in range(len(message)):
         result += ord(message[i])
     return result
+
+#http://cacr.uwaterloo.ca/hac/about/chap4.pdf
+#https://crypto.stackexchange.com/questions/1970/how-are-primes-generated-for-rsa
+def getPrime():
+    randNum = random.randrange(1e189, 9e189)
+    
+    while randNum % 2 != 0:
+        randNum = random.randrange(1e189, 9e189)
+        
+    #check for prime
+    return randNum
