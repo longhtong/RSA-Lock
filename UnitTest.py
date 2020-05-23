0aef3dacf4b88da5992ea3398fb08925c9bbe97c
Long Tong <longhtong@berkeley.edu>
2020-05-22 18:03:37 -0700

    prime generator finished

--- UnitTest.py
+++ UnitTest.py
@@ -1,8 +1,13 @@
 import encryption as en
 import decryption as de
 import Utils as utils
+lowerBound = 1e189
+upperBound = 9e189
 print(en.encrypt(13, 55, 3))
 print(de.decrypt(en.encrypt(13, 55, 3),3,5,11))
-print(utils.getPrime(3))
-
-print(utils.largest2Power(91))
+#print(utils.getPrime(3))
+x=1448253698229368010440434731715527436907444603619475643788436617178249755772639373106977420768228382693177505625573860309370280198474280439809533913663126983115003782748411347405597222712977
+#print(utils.largest2Power(91))
+#print(utils.miillerTest(x))
+#print(utils.MillerRabinPrime(x, -1))
+print(utils.getPrime(x, lowerBound, upperBound))
--- Utils.py
+++ Utils.py
@@ -1,6 +1,7 @@
 import random
 import decimal
-
+from egcd import egcd
+import math
 def ASCIIConvert(message):
     result = 0
     for i in range(len(message)):
@@ -9,50 +10,52 @@ def ASCIIConvert(message):
 
 #http://cacr.uwaterloo.ca/hac/about/chap4.pdf
 #https://crypto.stackexchange.com/questions/1970/how-are-primes-generated-for-rsa
-def getPrime(other):
-    limit = 3
-    prime = False
-
-    randNum = decimal.Decimal(random.randrange(1e189, 9e189))
-    while randNum % decimal.Decimal(2) == 0 or randNum == decimal.Decimal(other):
-        randNum = random.randrange(1e189, 9e189)
-
-    while not prime:
-        
-        #Get the largest 2^x factor that divides randNum
-        twoPowerResult = largest2Power(randNum - 1)
-        s = decimal.Decimal(twoPowerResult[1])
-        r = decimal.Decimal((randNum - 1) / twoPowerResult[0])
-
-        #check for prime
-        for i in range(limit):
-            a = random.randrange(2, randNum-2)
-            while a == other:
-                a = decimal.Decimal(random.randrange(2, randNum-2))
-            y = (a**r) % randNum
-            if y != 1 and y != (randNum-1):
-                j = 1
-                while j <= s-1 and y != (randNum-1):
-                    y = (y**2) % randNum
-                    if y == 1:
-                        prime = False
-                        break
-                    j += 1
-                if y != (randNum-1):
-                    prime = False
-            if prime == False:
-                randNum += 2
-                break
-        
-    return randNum
+def getPrime(other, lowerBound, upperBound):
+    while True:
+        randNum = random.randrange(lowerBound, upperBound)
+        while randNum % 2 == 0.0 or randNum == other:
+            randNum = random.randrange(1e189, 9e189)
+        if MillerRabinPrime(randNum, other):
+            return randNum
 
 #https://crypto.stanford.edu/pbc/notes/numbertheory/millerrabin.html
 def largest2Power(n):
     power = 1
-    possible_power = []
+    possible_power = [0]
     while (2**power) <= n:
         if (n % (2**power) == 0):
             possible_power.append(power)
         power += 1
-    return (2**max(possible_power), max(possible_power))
+    return (2**max(possible_power),max(possible_power))
+
+#https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test
+#http://cacr.uwaterloo.ca/hac/about/chap4.pdf
+def MillerRabinPrime(n, other):
+    limit = 3
+    #Get the largest 2^x factor that divides randNum
+    twoPowerResult = largest2Power(n - 1)
+    s = twoPowerResult[1]
+    r = (n - 1) // twoPowerResult[0]
+
+    #check for prime
+    for i in range(limit):
+        a = random.randrange(2, n-2)
+        while a == other:
+            a = random.randrange(2, n-2)
+        y = pow(a,r,n)
+        if y != 1 and y != (n-1):
+            j = 1
+            while j <= (s-1) and y != (n-1):
+                y = pow(y,2,n)
+                if y == 1:
+                    return False
+                j += 1
+            if y != (n-1):
+                return False
+    return True
+       
+
         
+
+  
+
