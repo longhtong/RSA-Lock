c391c8522d2587399d0f457c5864e1437df600ac
Long Tong <longhtong@berkeley.edu>
2020-05-31 12:47:27 -0700

    start GUI

---
+++ encryptorGUI.py
@@ -0,0 +1,19 @@
+import PySimpleGUI as sg
+
+sg.theme('Dark Blue 3')  # please make your windows colorful
+
+layout = [[sg.Button('Encrypt'), sg.Button('Decrypt')]]
+
+window = sg.Window('RSA Encryptor', layout)
+
+while True:  # Event Loop
+    event, values = window.read()
+    print(event, values)
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        break
+    if event == 'Encrypt':
+        # change the "output" element to be the value of "input" element
+        layout1 = [[sg.Button('New1'), sg.Button('New2')]]
+        window = sg.Window('RSA Encryptor', layout1)
+
+window.close()
